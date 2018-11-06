d94ece993d77f743df6b9230a408df2d1ddfe411
crycetruly <crycetruly@gmail.com>
2018-11-06 04:50:18 +0300

    added more order statuses

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,14 +1,25 @@
-let menu=document.querySelector('#menu');
-let menulink=document.querySelector('#menushow');
+newFunction();
 
-menulink.addEventListener('click',(e)=>{
-    console.log('clicked');
-    menu.classList.toggle('showing');
-e.preventDefault();
-});
-
-// bodywidth=document.body.clientWidth;
-// if(bodywidth<=535){
-//     menulink.addEventListener('click',function(){
-//         document.location='index.html';
-//     })
+function newFunction() {
+    let menu = document.querySelector('#menu');
+    let menulink = document.querySelector('#menushow');
+    menulink.addEventListener('click', (e) => {
+        console.log('clicked');
+        menu.classList.toggle('showing');
+        e.preventDefault();
+    });
+    // bodywidth=document.body.clientWidth;
+    // if(bodywidth<=535){
+    //     menulink.addEventListener('click',function(){
+    //         document.location='index.html';
+    //     })
+    // forgot navigation
+    try {
+        document.getElementById('fgtp').addEventListener('click', () => {
+           
+            document.location.href ="http://127.0.0.1:5500/UI/forgot.html";
+        });
+    }
+    catch (e) {
+    }
+}
