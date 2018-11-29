7e0d7413572f4bec1bf02acd755142eba19efa29
crycetruly <crycetruly@gmail.com>
2018-11-29 19:58:26 +0300

    validated userdata

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,25 +1,88 @@
-newFunction();
+    const baseURL="https://trulysendit.herokuapp.com/"
+    const erroutput=document.querySelector('#errors')
+    let btn=document.querySelector('#login');
+    btn.addEventListener('click',e=>{
+        errors=[]
+        let email=document.querySelector('#email').value
+        let password=document.querySelector('#password').value
+        if(is_valid_email(email)==false){
+            errors.push('email is invalid')   
+        }
+        if(password.length<6){
+            errors.push('password should be 6 characters')    
+        }
 
-function newFunction() {
-    let menu = document.querySelector('#menu');
-    let menulink = document.querySelector('#menushow');
-    menulink.addEventListener('click', (e) => {
-        console.log('clicked');
-        menu.classList.toggle('showing');
-        e.preventDefault();
-    });
-    // bodywidth=document.body.clientWidth;
-    // if(bodywidth<=535){
-    //     menulink.addEventListener('click',function(){
-    //         document.location='index.html';
-    //     })
-    // forgot navigation
-    try {
-        document.getElementById('fgtp').addEventListener('click', () => {
+        if(errors.length>0){
+          
+            errors.forEach(err => {
+            display_erors(err);
+                  
+            clear_errors();
            
-            document.location.href ="http://127.0.0.1:5500/UI/forgot.html";
-        });
+        })}else{
+            document.querySelector("body").classList.add("spinner-1");
+            data={
+                "password":email,
+                "password":password
+            }
+            startLogin(data)
+        }
+
+        
+
+        
+
+
+        e.preventDefault()
+        
+    })
+
+
+function is_valid_email(email) 
+    {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
     }
-    catch (e) {
-    }
+function startLogin(data){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    fetch(baseURL+"/api/v2/auth/login",
+     {
+        method: 'POST',
+        body:data,
+        headers:headers
+        
+        
+      }
+         
+    ).then(res=>{
+        document.querySelector("body").classList.remove("spinner-1");
+        console.log(res.data);
+        
+        display_erors(res.statusText)
+        setTimeout(() => {
+            
+        }, 1000);
+        clear_errors()
+        
+    }).catch(err=>{
+        document.querySelector("body").classList.remove("spinner-1");
+        console.log(err);
+    
+        
+    })
+}
+
+function display_erors(err){
+    erroutput.innerHTML+=`
+    <li>${err}</li>
+    `
+}
+
+function clear_errors(){
+    setTimeout(() => {
+                 
+        erroutput.innerHTML="";
+     }, 2000);
+       
 }
