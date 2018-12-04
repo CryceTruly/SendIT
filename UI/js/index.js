931efe8f81470281232dcfcbf983082320498d51
crycetruly <crycetruly@gmail.com>
2018-12-04 08:16:50 +0300

    hid links to content

--- UI/js/index.js
+++ UI/js/index.js
@@ -5,3 +5,12 @@ if(is_user_admin!==true){
 }else{
     document.querySelector("#is_admin").style.display='block';  
 }
+
+
+if(localStorage.getItem('_token')==null){
+    document.location.href='login.html?message=you need to login first';
+ }else{
+     document.querySelector("#loginli").style.display='none';
+     document.querySelector('#registerli').style.display='none';
+
+ }
