d720d7d2f5554520f4e753cbb78db242cb6d3161
crycetruly <crycetruly@gmail.com>
2018-12-06 08:20:59 +0300

    added order printing

--- js/parcel.js
+++ js/parcel.js
@@ -10,10 +10,13 @@ fetch(baseURL + `parcels/${current_item}`, {
 })
     .then(response => response.json())
     .then(data => {
+        console.log(data);
+        
       document.querySelector(".new").innerHTML=`          Created                  ${data.created}`;
       document.querySelector(".id").innerHTML=`Order #${data.parcel_id}`;
           document.querySelector(".oneleft").classList.remove("spinner-1");
           document.querySelector(".oneleft").innerHTML=`
+          
           <h4><strong>Pick Up Location Address</strong></h4>
           <p>${data.pickup_address}</p>
 
@@ -38,16 +41,22 @@ fetch(baseURL + `parcels/${current_item}`, {
           <h4>Total Weight</h4>
           <span>${data.weight} kg</span>
 
+          <h4>Sender Address</h4>
+          <span>${data.sender_email}</span>
+          <h4>Recipient Address</h4>
+          <span>${data.recipient_email}</span>
+
           <h4>Total Price</h4>
-          <span>${data.price}</span>
+          <span>${data.price} USD</span>
+
+          </div>
 
 
           <div class="row first">
           <div class="column">
-              <button class="button" id=cancelbtn onclick=cancelOrder(${data.parcel_id})>Cancel Order</button>
-
-          </div>
-          <div class="column">
+             <button class="button" id=printbtn onclick=printOrder()>Print Order</button>
+              <button class="button" id=cancelbtn onclick=cancelOrder("${data.status}")>Cancel Order</button>
+              <button class="button" id=deletebtn onclick=deleteOrder(${data.parcel_id})>Delete Order</button>
               <button class="button btn-info"><a href=changedest.html?parcel=${data.parcel_id}>Change Destination Address</a></button>
           </div>
       </div>
@@ -61,7 +70,15 @@ fetch(baseURL + `parcels/${current_item}`, {
 
     })
 
-function cancelOrder(id){
+function cancelOrder(status){
+    console.log(status);
+    
+   
+    
+    if(status==='cancelled'){
+        alert('order already cancelled')
+        return false;
+    }
     let userinput=confirm('are you sure you want to cancel this order?')
     if(userinput){
         fetch(baseURL +`parcels/${current_item}/cancel`, {
@@ -70,7 +87,8 @@ function cancelOrder(id){
             .then(response => response.json())
             .then(jsondata => {
                 document.querySelector("body").classList.remove("spinner-1");     
-    alert('item cancelled')
+                alert('item cancelled')
+                document.location.href='profile.html';
     
     
             })
@@ -79,4 +97,40 @@ function cancelOrder(id){
     }
     
     
+}
+
+
+function deleteOrder(id){
+    let userinput=confirm('are you sure you want to delete this order?')
+    if(userinput){
+        fetch(baseURL +`parcels/${current_item}/delete`, {
+            method: "DELETE", headers: headers
+        })
+            .then(response => response.json())
+            .then(jsondata => {
+                document.querySelector("body").classList.remove("spinner-1");     
+                alert('item deleted')
+                document.location.href='profile.html';
+    
+    
+            }).catch(err=>console.log(err)
+            )
+        
+        
+    }
+
+}
+
+function printOrder(){
+    document.querySelector(".first").innerHTML=`<hr>
+    <i><b>SendIT</b> parcel delivery services</i>
+    `;
+    var printContents = document.getElementById('printsection').innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
 }
