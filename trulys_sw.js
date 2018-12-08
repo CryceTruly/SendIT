3818b4212e2da96aba1113a8e257f7cd6e75e6a4
crycetruly <crycetruly@gmail.com>
2018-12-08 13:13:06 +0300

    cached the api requests

---
+++ trulys_sw.js
@@ -0,0 +1,47 @@
+const cacheName = 'trulysCache';
+// #install
+self.addEventListener('install',(e)=>{
+    
+})
+
+// activate
+self.addEventListener('activate',(e)=>{  
+        // Remove unwanted caches
+        e.waitUntil(
+          caches.keys().then(cacheNames => {
+            return Promise.all(
+              cacheNames.map(cache => {
+                if (cache !== cacheName) {
+                  return caches.delete(cache);
+                }
+              })
+            );
+          })
+        );
+      });
+      
+      // Call Fetch Event
+      self.addEventListener('fetch', e => {
+        e.respondWith(
+          fetch(e.request)
+            .then(res => {
+              // Make copy/clone of response
+              const resClone = res.clone();
+              // Open cahce
+              caches.open(cacheName).then(cache => {
+                // Add response to cache
+                cache.put(e.request, resClone);
+              });
+              return res;
+            })
+            .catch(err => caches.match(e.request).then(res => res))
+        );
+      });
+  
+  
+  
+  
+  
+  
+  
+  
