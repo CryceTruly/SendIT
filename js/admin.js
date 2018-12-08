6905f2eef45d283ebb00e503b63cb7e18cda4ea9
crycetruly <crycetruly@gmail.com>
2018-12-08 13:18:46 +0300

    rewrote evenlistener functions to one fn

--- js/admin.js
+++ js/admin.js
@@ -2,7 +2,7 @@ const baseURL="http://127.0.0.1:3000/api/v2/";
 if(localStorage.getItem('_token')==null){
     document.location.href='../login.html?message=you need to log in first';
  }
- if(localStorage.getItem('is_admin')===true){
+ if(localStorage.getItem('is_admin')!=='true'){
     document.location.href='../index.html?message=you cannot access that';
  }
 document.querySelector("tbody").classList.add("spinner-1");
@@ -58,10 +58,20 @@ fetch(baseURL + "parcels", {
         document.querySelector('#in').textContent=`${in_transit}`;
         document.querySelector('#pen').textContent=`${pending}`;
 
-        document.querySelector('.container2').addEventListener('click',filterToCancelled)
-        document.querySelector('.container3').addEventListener('click',filterToDelivered)
-        document.querySelector('.container5').addEventListener('click',filterToPending)
-        document.querySelector('.container4').addEventListener('click',filterToIntransit)
+     
+
+        document.querySelector('.container2').addEventListener('click',function(){
+            return filterTableCategories('cancelled',cancelled);
+        });
+        document.querySelector('.container3').addEventListener('click',function(){
+            return filterTableCategories('delivered',delivered);
+        });
+        document.querySelector('.container5').addEventListener('click',function(){
+            return filterTableCategories('order_placed',pending);
+        });
+        document.querySelector('.container4').addEventListener('click',function(){
+            return filterTableCategories('in_transit',in_transit);
+        });
         document.querySelector('.container1').addEventListener('click',function(){
             document.location.reload();
         })
@@ -74,110 +84,40 @@ fetch(baseURL + "parcels", {
     })
 
 
+ 
+
+    function filterTableCategories(status,count){
+        document.querySelector('tbody').innerHTML="";
+        document.querySelector('tbody').classList.add('spinner-1');
+
+        if(count==0)[
+            document.querySelector('tbody').innerHTML=`No parcels of status `+status
+        ]
+        allparcels.forEach(parcel=>{
+        if(parcel['status']==status){
+            document.querySelector('tbody').innerHTML+=`
+            <tr>
+            <td>${parcel.parcel_id}</td>
+            <td>${parcel.sender_email}</td>
+            <td>${parcel.pickup_address}</td>
+            <td>${parcel.destination_address}</td>
+            <td>${formatDate(parcel.placed)}</td>
+            <td><a href=../details.html?parcel=${parcel.parcel_id}><i class="fa fa-long-arrow-right"></i></a></td>
+            <td><a href="../editorder.html?order=${parcel.parcel_id}"><i class="fa fa-pencil"></i></a></td>
+        </tr>
+            `
+        
+            }
+            setTimeout(() => {
+                document.querySelector('tbody').classList.remove('spinner-1');
+            }, 1000);
+        
+        })
+
+        
+
+    }
+
     function formatDate(date){
         return date.split('00')[0];
     }
-
-    function filterToCancelled(){
-        document.querySelector('tbody').innerHTML="";
-        document.querySelector('tbody').classList.add('spinner-1');
-        allparcels.forEach(parcel=>{
-        if(parcel['status']=='cancelled'){
-            document.querySelector('tbody').innerHTML+=`
-            <tr>
-            <td>${parcel.parcel_id}</td>
-            <td>${parcel.sender_email}</td>
-            <td>${parcel.pickup_address}</td>
-            <td>${parcel.destination_address}</td>
-            <td>${formatDate(parcel.placed)}</td>
-            <td><a href=../details.html?parcel=${parcel.parcel_id}><i class="fa fa-long-arrow-right"></i></a></td>
-            <td><a href="../editorder.html?order=${parcel.parcel_id}"><i class="fa fa-pencil"></i></a></td>
-        </tr>
-            `
-        
-            }
-            setTimeout(() => {
-                document.querySelector('tbody').classList.remove('spinner-1');
-            }, 1000);
-        
-        })
-
-    }
-    
-    function filterToDelivered(){
-        document.querySelector('tbody').innerHTML="";
-        document.querySelector('tbody').classList.add('spinner-1');
-        allparcels.forEach(parcel=>{
-        if(parcel['status']=='delivered'){
-            document.querySelector('tbody').innerHTML+=`
-            <tr>
-            <td>${parcel.parcel_id}</td>
-            <td>${parcel.sender_email}</td>
-            <td>${parcel.pickup_address}</td>
-            <td>${parcel.destination_address}</td>
-            <td>${formatDate(parcel.placed)}</td>
-            <td><a href=../details.html?parcel=${parcel.parcel_id}><i class="fa fa-long-arrow-right"></i></a></td>
-            <td><a href="../editorder.html?order=${parcel.parcel_id}"><i class="fa fa-pencil"></i></a></td>
-        </tr>
-            `
-        
-            }
-            setTimeout(() => {
-                document.querySelector('tbody').classList.remove('spinner-1');
-            }, 1000);
-        
-        })
-
-    }
-    function filterToIntransit(){
-        document.querySelector('tbody').innerHTML="";
-        document.querySelector('tbody').classList.add('spinner-1');
-        allparcels.forEach(parcel=>{
-        if(parcel['status']=='in_transit'){
-            document.querySelector('tbody').innerHTML+=`
-            <tr>
-            <td>${parcel.parcel_id}</td>
-            <td>${parcel.sender_email}</td>
-            <td>${parcel.pickup_address}</td>
-            <td>${parcel.destination_address}</td>
-            <td>${formatDate(parcel.placed)}</td>
-            <td><a href=../details.html?parcel=${parcel.parcel_id}><i class="fa fa-long-arrow-right"></i></a></td>
-            <td><a href="../editorder.html?order=${parcel.parcel_id}"><i class="fa fa-pencil"></i></a></td>
-        </tr>
-            `
-        
-            }
-            setTimeout(() => {
-                document.querySelector('tbody').classList.remove('spinner-1');
-            }, 1000);
-        
-        })
-
-    }
-
-    function filterToPending(){
-        document.querySelector('tbody').innerHTML="";
-        document.querySelector('tbody').classList.add('spinner-1');
-        allparcels.forEach(parcel=>{
-        if(parcel['status']=='order_placed'){
-            document.querySelector('tbody').innerHTML+=`
-            <tr>
-            <td>${parcel.parcel_id}</td>
-            <td>${parcel.sender_email}</td>
-            <td>${parcel.pickup_address}</td>
-            <td>${parcel.destination_address}</td>
-            <td>${formatDate(parcel.placed)}</td>
-            <td><a href=../details.html?parcel=${parcel.parcel_id}><i class="fa fa-long-arrow-right"></i></a></td>
-            <td><a href="../editorder.html?order=${parcel.parcel_id}"><i class="fa fa-pencil"></i></a></td>
-        </tr>
-            `
-        
-            }
-            setTimeout(() => {
-                document.querySelector('tbody').classList.remove('spinner-1');
-            }, 1000);
-        
-        })
-
-    }
-
